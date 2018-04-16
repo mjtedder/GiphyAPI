4b4ef8bd0e90a6026384b5d5c259b873f7f496c9
unknown <mjtedder@gmail.com>
2018-04-15 23:54:45 -0400

    HTML shell done.  Psuedocode done

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//Let's pseudocode what we want to do first
+
+//STEP ONE:  Let's link to the GIPHY API using our API Key
+
+//STEP TWO: Let's console log it to ensure everything is linked properly
+
+//STEP THREE: Let's make an array of strings for our topic (reactions)
+  //Save it to a variable call topics
+
+//STEP FOUR: Let's get functionality for when our buttons are pressed to display images
+  //Use a loop that appends a button for each string in the array
+
+//STEP FIVE: Let's make an AJAX call using jQuery to grab 10 static images for each button
+
+//STEP SIX: Let's add functionality to pause/play our gifs when clicked
+
+//STEP SEVEN: Let's display the rating under the GIFs
+
+//STEP EIGHT: Let's create a form where the user can type a button they want to add
+
+//STEP NINE: Let's use jQuery to create a new button when the submit button is clicked
+
+//Don't forget to reference Activites in Module 6 from class and the GIPHY API DOCS!!!
